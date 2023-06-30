c689dbaaa92fb101fe2bb1e8d137535519a25a2c
XU, Hui <china.xuhui@gmail.com>
2023-06-30 12:42:12 +0800

    Update 1-ptr-simple-unsafe-low.rs

--- knowledgebase/add/1-ptr-simple-unsafe-low.rs
+++ knowledgebase/add/1-ptr-simple-unsafe-low.rs
@@ -1,9 +1,8 @@
-#![allow(unused)]
-fn main() {
-    let s: &str = "123";
-    let ptr: *const u8 = s.as_ptr();
-
-    unsafe {
-        *ptr.add(1);
-    }
+//#![allow(unused)]
+//fn main() {
+let s: &str = "123";
+let ptr: *const u8 = s.as_ptr();
+unsafe {
+    *ptr.add(1);
 }
+//}
