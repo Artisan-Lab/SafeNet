d8c5289b35d57e937f0448c63e90d1151ac84a3e
XU, Hui <china.xuhui@gmail.com>
2023-07-01 08:56:37 +0800

    Create 1-vec2rc-unsafe.rs

---
+++ knowledgebase/Rc::from_raw/1-vec2rc-unsafe.rs
@@ -0,0 +1,9 @@
+//use std::rc::Rc;
+
+//fn main() {
+    let v = vec![1,2,3];
+    let p = Box::into_raw(Box::new(v));
+    unsafe { (*p)[1] = 24; }
+    let rc = unsafe { Rc::from_raw(p) };
+    assert_eq!((*rc)[1], 24);
+//}
