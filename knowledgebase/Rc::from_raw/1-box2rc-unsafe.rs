50d4238e4488648995baeec778e1f2be3f8108e4
XU, Hui <china.xuhui@gmail.com>
2023-07-01 11:11:32 +0800

    Update 1-box2rc-unsafe.rs

--- knowledgebase/Rc::from_raw/1-box2rc-unsafe.rs
+++ knowledgebase/Rc::from_raw/1-box2rc-unsafe.rs
@@ -10,10 +10,10 @@ struct MyStruct {
     value: i32,
 }
 */
-//fn main() {
-let b = Box::new( MyStruct { value: 42 });
-let p: *mut MyStruct = Box::into_raw(b);
-unsafe { (*p).value = 24; }
-let rc = unsafe { Rc::from_raw(p) };
-//println!("Value: {}", rc.value); // Output: Value: 24
-//}
+fn main() {
+    let b = Box::new( MyStruct { value: 42 });
+    let p: *mut MyStruct = Box::into_raw(b);
+    unsafe { (*p).value = 24; }
+    let rc = unsafe { Rc::from_raw(p) };
+    //println!("Value: {}", rc.value); // Output: Value: 24
+}
