bef9ad2a6bbddec5fd6025c9445b34236b91fc62
XU, Hui <china.xuhui@gmail.com>
2023-07-01 11:11:11 +0800

    Update 1-String2rc-unsafe.rs

--- knowledgebase/Rc::from_raw/1-String2rc-unsafe.rs
+++ knowledgebase/Rc::from_raw/1-String2rc-unsafe.rs
@@ -5,11 +5,11 @@
 
 //use std::rc::Rc;
 
-//fn main() {
+fn main() {
     let mut s = String::from("123");
     s.push('s');
     s.push_str("123");
     let p = Box::into_raw(Box::new(s)); 
     let rc = unsafe { Rc::from_raw(p) };
     println!("{}", *rc);
-//}
+}
