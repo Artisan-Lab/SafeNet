8b38456dcf2969ee6781c9949094e9d6f38531dc
XU, Hui <china.xuhui@gmail.com>
2023-07-01 21:29:47 +0800

    update

--- testdata/Arc__decrement_strong_count_1/13.rs
+++
@@ -1,6 +0,0 @@
-pub unsafe extern "C" fn decrement_arc(arc_ptr: u64) {
-    Arc::decrement_strong_count(arc_ptr as usize as *const Vec<u8>);
-}
-/*
-https://github.com/swyxio/zaplib/blob/55820affcee3f2761e0e2f9e799fdf039874610a/zaplib/main/src/cx_wasm32.rs#L854
-*/
