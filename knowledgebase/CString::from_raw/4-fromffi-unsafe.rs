043a9021dbe5eeb7b587f173bb80ef5d7bbe66df
XU, Hui <china.xuhui@gmail.com>
2023-07-02 11:13:50 +0800

    update

---
+++ knowledgebase/CString::from_raw/4-fromffi-unsafe.rs
@@ -0,0 +1,13 @@
+#![allow(unused)]
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+fn foo() -> *mut c_char {
+    let mut cstring = CString::new("hello").expect("Failed to create CString");
+    cstring.into_raw() as *mut c_char
+}  
+
+fn main() {
+    let raw = foo();
+    unsafe { CString::from_raw(raw); }
+}
