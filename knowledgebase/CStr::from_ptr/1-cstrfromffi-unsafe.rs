59bfd03916bf92f24e44b4711481b3982952c801
XU, Hui <china.xuhui@gmail.com>
2023-07-02 10:53:31 +0800

    update

--- knowledgebase/CStr::from_ptr/1-cstrfromffi-unsafe.rs
+++ knowledgebase/CStr::from_ptr/1-cstrfromffi-unsafe.rs
@@ -1,11 +1,12 @@
 #![allow(unused)]
 use std::ffi::{c_char, CStr};
 
-extern "C" {
-    fn my_string() -> *const c_char;
-}
+
 
 fn main() {
+    extern "C" {
+        fn my_string() -> *const c_char;
+    }
     let raw = unsafe { my_string() };
     let cstr = unsafe { CStr::from_ptr(raw) };
 }
