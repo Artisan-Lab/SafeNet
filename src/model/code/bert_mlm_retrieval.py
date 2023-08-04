e45baa68d0684b0649ec725875ca9f6992cab855
DDDdyan <dddoofdu@163.com>
2023-08-04 11:21:41 +0800

    updata

--- src/model/code/bert_mlm_retrieval.py
+++ src/model/code/bert_mlm_retrieval.py
@@ -5,8 +5,6 @@ import json
 from tqdm import tqdm
 import os
 import heapq
-import torch
-import torch.nn.functional as F
 
 def encode(document: str) -> torch.Tensor:
     tokens = tokenizer(document, return_tensors='pt', max_length=256, truncation=True)
@@ -52,40 +50,63 @@ if __name__ == "__main__":
                     attention_mask_1 = torch.tensor(tokenized_input1['attention_mask']).unsqueeze(0)
                     input_ids_2 = torch.tensor(tokenized_input2['input_ids']).unsqueeze(0)
                     attention_mask_2 = torch.tensor(tokenized_input2['attention_mask']).unsqueeze(0)
-
-                    query_cos = model(input_ids_1, attention_mask_1, input_ids_2, attention_mask_2)
-                    tensor1 = query_cos[0]
-                    tensor2 = query_cos[1]
-                    cosine_similarity = F.cosine_similarity(tensor1, tensor2, dim=1)
+                    embed1, embed2 = model(input_ids_1, attention_mask_1, input_ids_2, attention_mask_2)
+                    cosine_similarity = torch.nn.functional.cosine_similarity(embed1, embed2)
                     Similarity.append(cosine_similarity.item())
-            
-                # 使用堆数据结构获取相似度最高的3个值及其对应的索引
-                top_k_similarities = heapq.nlargest(3, Similarity)
-                top_k_indices = [Similarity.index(similarity) for similarity in top_k_similarities]
 
-                top_k_results = []
-                for index in top_k_indices:
-                    most_similar_text_id = category_vectors_base[sample['category']][index][1]
-                    most_similar_category = category_vectors_base[sample['category']][index][2]
-                    similarity_value = Similarity[index]
+                max_similarity_index = Similarity.index(max(Similarity))
+                most_similar_text_id = category_vectors_base[sample['category']][max_similarity_index][1]
+                most_similar_category = category_vectors_base[sample['category']][max_similarity_index][2]
 
-                    top_k_results.append({
-                        'predict': most_similar_text_id,
-                        'category': most_similar_category,
-                        'Similarity_value': similarity_value
-                    })
-
-                result = {
-                    'id': sample['id'],
-                    'text': sample['text'],
-                    'category': sample['category'],
-                    'similarity': sample['similarity'],
-                    'top_k_results': top_k_results
-                }
+                result = {}
+                result['id'] = sample['id']
+                result['text'] = sample['text']
+                result['category'] = sample['category']
+                result['similarity'] = sample['similarity']
+                result['predict'] = most_similar_text_id
+                result['Similarity_value'] = max(Similarity)
                 results.append(result)
-
             f.write(json.dumps(results, ensure_ascii=False, indent=4) + '\n')
-                
+
+
+/* 
+        // top3
+        with open('../user_data/results/result.json', 'w', encoding='utf-8-sig') as f:
+            results = []
+            for idx, sample in enumerate(tqdm(test)):
+                Similarity = []
+                indices = []
+                for base in category_vectors_base[sample['category']]:
+                    tokenized_input1 = tokenizer(sample['text'], truncation=True, max_length=100, pad_to_max_length=True)
+                    tokenized_input2 = tokenizer(base[0], truncation=True, max_length=100, pad_to_max_length=True)
+
+                    input_ids_1 = torch.tensor(tokenized_input1['input_ids']).unsqueeze(0)
+                    attention_mask_1 = torch.tensor(tokenized_input1['attention_mask']).unsqueeze(0)
+                    input_ids_2 = torch.tensor(tokenized_input2['input_ids']).unsqueeze(0)
+                    attention_mask_2 = torch.tensor(tokenized_input2['attention_mask']).unsqueeze(0)
+
+                    embed1, embed2 = model(input_ids_1, attention_mask_1, input_ids_2, attention_mask_2)
+
+                    lp_distance = torch.norm(embed1 - embed2, p=p, dim=1)
+                    similarity = 1 / (1 + lp_distance)
+
+                    Similarity.append(similarity.item())
+                    indices.append(base[1])  # 添加相似度对应的index值
+
+                # 使用堆来获取相似度最大的前三个结果
+                top_similarities = heapq.nlargest(1, zip(Similarity, indices))
+
+                result = {}
+                result['id'] = sample['id']
+                result['text'] = sample['text']
+                result['category'] = sample['category']
+                result['similarity'] = sample['similarity']
+                result['predict'] = [x[1] for x in top_similarities]
+                result['Similarity_value'] = [x[0] for x in top_similarities]
+                results.append(result)
+            f.write(json.dumps(results, ensure_ascii=False, indent=4) + '\n')
+
+*/
 
 
 
