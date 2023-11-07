4eb943fba45578c718936d003698b3e02e7c82ec
DDDdyan <dddoofdu@163.com>
2023-11-07 11:40:21 +0800

    update

--- knowledgebase/mem::zeroed/2-maybeuninit-unsafe.rs
+++ knowledgebase/mem::zeroed/2-maybeuninit-unsafe.rs
@@ -1,19 +1,27 @@
-use std::mem;
+use std::mem::MaybeUninit;
 
 #[derive(Debug)]
 struct MyStruct<'a> {
     number: i32,
     flag: bool,
-    b: &'a Box<i32>,
+    b: Option<&'a Box<i32>>,
 }
 
 fn foo<'a>(x: *mut MyStruct<'a>, y: &'a Box<i32>) {
-    unsafe {(*x).b = y; }
+    unsafe { (*x).b = Some(y); }
 }
 
 fn main() {
-    // Create an instance of MyStruct initialized with zeroes
-    let mut myst: MyStruct = unsafe { mem::zeroed() };
+    let mut myst: MyStruct = unsafe {
+        let number = MaybeUninit::zeroed();
+        let flag = MaybeUninit::zeroed();
+        MyStruct {
+            number: number.assume_init(),
+            flag: flag.assume_init(),
+            b: None,
+        }
+    };
+
     let b = Box::new(1);
     foo(&mut myst as *mut _, &b);
     println!("{:?}", myst);
