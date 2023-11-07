669a065ff14cf9081ce90b6e79c613676a5b1cac
DDDdyan <90452649+DDDdyan@users.noreply.github.com>
2023-11-07 11:28:36 +0800

    Update 2-maybeuninit-unsafe.rs

--- knowledgebase/mem::zeroed/2-maybeuninit-unsafe.rs
+++ knowledgebase/mem::zeroed/2-maybeuninit-unsafe.rs
@@ -1,31 +1,28 @@
 use std::mem::MaybeUninit;
-use std::mem::ManuallyDrop;
 
 #[derive(Debug)]
 struct MyStruct<'a> {
     number: i32,
     flag: bool,
-    b: &'a Box<i32>,
+    b: Option<&'a Box<i32>>,
 }
 
 fn foo<'a>(x: *mut MyStruct<'a>, y: &'a Box<i32>) {
-    unsafe { (*x).b = y; }
+    unsafe { (*x).b = Some(y); }
 }
 
 fn main() {
-    let mut myst: ManuallyDrop<MyStruct> = ManuallyDrop::new(unsafe {
+    let mut myst: MyStruct = unsafe {
         let number = MaybeUninit::zeroed();
         let flag = MaybeUninit::zeroed();
-        let b: *const Box<i32> = std::ptr::null(); 
-
         MyStruct {
             number: number.assume_init(),
             flag: flag.assume_init(),
-            b:&*b ,
+            b: None,
         }
-    });
+    };
 
     let b = Box::new(1);
-    foo(&mut *myst as *mut _, &b);
-    println!("{:?}", myst );
+    foo(&mut myst as *mut _, &b);
+    println!("{:?}", myst);
 }
